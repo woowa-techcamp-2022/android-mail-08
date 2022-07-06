b354fb1c2abc5a70b9a127c363092505bb6735fa
woowahan <woowahan@woowahan-MacBook-Pro.local>
2022-07-06 16:02:05 +0900

    [Feat] 로그인 xml 완성

--- app/src/main/java/com/example/mailappproject/MainActivity.kt
+++ app/src/main/java/com/example/mailappproject/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.mailappproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mailappproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initLayout()
+    }
+
+    private fun initLayout() {
+
     }
 }
--- app/src/main/java/com/example/mailappproject/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/mailappproject/presentation/login/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.example.mailappproject.presentation.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mailappproject.R
+import com.example.mailappproject.databinding.ActivityLoginBinding
+import com.example.mailappproject.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }
